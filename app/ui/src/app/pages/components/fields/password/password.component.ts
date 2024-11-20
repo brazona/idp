13b877b5ff391d8d8934d11db2c5d9055da2d6d3
Cezar Silva <cezarfelipedasilva@gmail.com>
2024-11-19 21:17:54 -0300

    feat: implementado lógica para o componente password

--- app/ui/src/app/pages/components/fields/password/password.component.ts
+++ app/ui/src/app/pages/components/fields/password/password.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectionStrategy, Component, inject, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ElementRef, signal} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -6,7 +6,8 @@ import {MatSelectModule} from '@angular/material/select';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-field-password',
@@ -18,12 +19,14 @@ import { CommonModule } from '@angular/common';
 })
 export class PasswordComponent {
   passVisibility:boolean = false;
-  hide = signal(true);
-  clickEvent(event: MouseEvent) {
-    this.hide.set(!this.hide());
-    event.stopPropagation();
-  }
   showPass(){
     this.passVisibility = !this.passVisibility;
+    const password = document.querySelector(".field-password");
+    if (password)
+      this.setType(password);
+  }
+  private setType(field:Element){
+    const type = field.getAttribute('type') === 'password' ? 'text' : 'password'; 
+    field.setAttribute('type', type)
   }
 }
