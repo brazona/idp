db6fd814093c621986023e8077ec790f950399bb
Cezar Silva <cezarfelipe2008@outlook.com>
2025-01-13 22:33:39 -0300

    feat: Front end - Codificação do Designer Gráfico

---
+++ app/ui/src/app/pages/recuperacao/atualizacao/atualizacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atualizacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './atualizacao.component.html',
+  styleUrl: './atualizacao.component.scss'
+})
+export class AtualizacaoComponent {
+
+}
---
+++ app/ui/src/app/pages/recuperacao/validacao/validacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './validacao.component.html',
+  styleUrl: './validacao.component.scss'
+})
+export class ValidacaoComponent {
+
+}
