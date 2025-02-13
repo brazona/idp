af20ee1797cdf572badcdaa331c7f8f5ca284a47
Cezar Silva <cezarfelipe2008@outlook.com>
2025-02-13 20:35:33 -0300

    feat: inclusão do origin cors na variavel de ambiente

--- app/api/src/main/java/br/brazona/idp/api/infrastructure/config/security/CustomCorsConfiguration.java
+++ app/api/src/main/java/br/brazona/idp/api/infrastructure/config/security/CustomCorsConfiguration.java
@@ -1,6 +1,8 @@
 package br.brazona.idp.api.infrastructure.config.security;
 
+import br.brazona.idp.api.domain.utils.EnvUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -10,9 +12,9 @@ import java.util.List;
 
 @Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
-    private static final String[] ORIGIN = {
-            "http://localhost:4200" , "http://127.0.0.1:4200"
-    };
+    @Autowired
+    private EnvUtil envUtil;
+    private static final String CORS_ORIGIN_PROP = "idp.cors.origin";
     /**
      * Return a {@link CorsConfiguration} based on the incoming request.
      *
@@ -26,12 +28,14 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
 
         corsConfig.setAllowedHeaders(List.of("*"));
         corsConfig.setMaxAge(3600L);
+        String value = envUtil.getEnvValue(CORS_ORIGIN_PROP);
+        String[] list_cors_origin = value.split(",");
 
         if ("OPTIONS".equals(request.getMethod())) {
             corsConfig.setAllowedOrigins(List.of(request.getHeader("Origin")));
             corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH", "OPTIONS"));
         } else {
-            corsConfig.setAllowedOrigins(List.of(ORIGIN));
+            corsConfig.setAllowedOrigins(List.of(list_cors_origin));
             corsConfig.setAllowCredentials(true);
             corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE"));
         }
